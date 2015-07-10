1707860f6dd48616b12b5133be361c319f165b94
Bruno Barreto <bruno@brunobarreto.net>
2015-07-10 12:36:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+exports.handler = function(event, context) {
+
+  var request = require('request');
+
+  var auth = new Buffer(event.user + ":" + event.password).toString('base64');
+
+  request(
+      {
+        method: 'POST',
+        uri: 'https://api-rest.zenvia360.com.br/services/send-sms/',
+
+        headers: {
+          'Authorization': 'Basic '+ auth,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+
+        body: JSON.stringify({
+            'sendSmsRequest': {
+              'from': event.from,
+              'to': event.to,
+              'msg': event.msg
+            }
+        })
+      },
+
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              resposta = JSON.parse(body);
+              context.succeed();
+          } else {
+            context.fail({error: error, response:response, body: body });
+          }
+      }
+  );
+
+};
